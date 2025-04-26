8e59ae957982863a38d1f92a0ac3efe5d74ceaf9
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2025-04-26 22:14:35 +0200

    add support for pet models

--- src/main/java/com/runeprofile/data/PlayerModelData.java
+++ src/main/java/com/runeprofile/data/PlayerModelData.java
@@ -4,9 +4,13 @@ package com.runeprofile.data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.annotation.Nullable;
+
 @RequiredArgsConstructor
 @Getter
 public class PlayerModelData {
-	private final String accountHash;
-	private final byte[] model;
+    private final String accountHash;
+    private final byte[] model;
+    @Nullable
+    private final byte[] petModel;
 }
